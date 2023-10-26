0cb6ca826976e9c8e45eea8ac89fdef026879039
n5upi <dallasats@hotmail.com>
2023-10-26 03:55:11 -0400

    Adding temporary changes to Github

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,31 +9,28 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
+ #  path for dealer reviews view
     path(route='', view=views.get_dealerships, name='index'),
- #   path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
- #   path('dealer/<int:id>/review', views.add_review, name='add_review'),
-    path(route='about/', view=views.about, name='about'),
-    path(route='contact/', view=views.contact, name='contact'),
- #   path('registration/', views.registration_request, name='registration'),
- #   path('login/', views.login_request, name='login'),
- #   path('logout/', views.logout_request, name='logout'),
 
-    # path for about view
-    #path(route='', view=views.get_about, name='about'),
+ #   path for dealer details
+ #   path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+
+ #   path for add a review view
+ #   path('dealer/<int:id>/review', views.add_review, name='add_review'),
+
+    # path for about us view
+ #   path(route='about/', view=views.about, name='about'),
 
     # path for contact us view
-    #path(route='', view=views.contact, name='contact'),
+ #   path(route='contact/', view=views.contact, name='contact'),
 
-    # path for registration
+ #   path for registration
+ #   path('registration/', views.registration_request, name='registration'),
 
-    # path for login
+ #   path for login
+ #   path('login/', views.login_request, name='login'),
 
-    # path for logout
-
-    # path(route='', view=views.get_dealerships, name='index'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
+ #   path for logout
+ #   path('logout/', views.logout_request, name='logout'),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
